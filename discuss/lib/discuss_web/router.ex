0ff9ce4bd0151a1e08568ca7379a4353f10925be
mooping <mongkonchai4412@gmail.com>
2018-08-09 14:50:37 +0700

    add discuss

---
+++ discuss/lib/discuss_web/router.ex
@@ -0,0 +1,36 @@
+defmodule DiscussWeb.Router do
+  use DiscussWeb, :router
+
+  pipeline :browser do
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(DiscussWeb.Plugs.SetUser)
+  end
+
+  pipeline :api do
+    plug(:accepts, ["json"])
+  end
+
+  scope "/", DiscussWeb do
+    # Use the default browser stack
+    pipe_through(:browser)
+
+    resources("/", TopicController)
+  end
+
+  scope "/auth", DiscussWeb do
+    pipe_through(:browser)
+
+    get("/signout", AuthController, :signout)
+    get("/:provider", AuthController, :request)
+    get("/:provider/callback", AuthController, :callback)
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", DiscussWeb do
+  #   pipe_through :api
+  # end
+end
